a1a4705129b73c89b1067a5cda53cb8dbe3eb62a
Akram <akrambenaoun1993@gmail.com>
2025-04-21 20:50:10 +0100

    add environment and routes files

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-
-const userRoutes = require('./routes/routes');
-
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+
+const Routes = require('./routes/routes.js');
 
 app.use(cors());
+
 app.use(express.json());
 
-// app.use('/api/', userRoutes);
-
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send('Hello from the Node.js backend!');
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use('/api', Routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
