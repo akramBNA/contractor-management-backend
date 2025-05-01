adc50a210b4cf3339dcf0ab4d0ec7c4631c98ef8
Akram <akrambenaoun1993@gmail.com>
2025-05-01 18:33:58 +0100

    contracts MVC

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const users_controller = require("../controllers/users.controllers");
 const roles_controller = require("../controllers/roles.controllers");
 const employees_controller = require("../controllers/employees.controlers");
+const contractsControlers = require("../controllers/contracts.controlers");
+
 
 // USERS ROUTES.
 router.get("/users/getAllUsers/", users_controller.getAllUsers);
@@ -13,5 +15,9 @@ router.get("/roles/getAllRoles/", roles_controller.getAllRoles);
 
 // EMPLOYEES ROUTES
 router.get("/employees/getAllEmployees/", employees_controller.getAllemployees);
+
+// CONTRACTS ROUTES
+router.get("/contracts/getAllContracts/", contractsControlers.getAllContracts);
+
 
 module.exports = router;
