d17ad7c79aaadebfb2479c419cdac1062ec0af86
Akram <akrambenaoun1993@gmail.com>
2025-06-18 22:52:07 +0100

    implement a getAllAssignedEmployees() mvc method

--- controllers/mission_employees.controllers.js
+++ controllers/mission_employees.controllers.js
@@ -4,5 +4,7 @@ const mission_EmployeesDao = require("../dao/mission_employees.dao");
 const mission_employees_instance = new mission_EmployeesDao();
 
 module.exports = {
-    
+    getAllAssignedEmployees: function (req, res, next) {
+        mission_employees_instance.getAllAssignedEmployees(req, res, next);
+    }
 };
