583397347d0d2e5a3c6e006bc05389c836a0a2bf
Akram <akrambenaoun1993@gmail.com>
2025-06-18 22:48:33 +0100

    getAllAssignedEmployees() dao

--- dao/mission_employees.dao.js
+++ dao/mission_employees.dao.js
@@ -1,7 +1,31 @@
 const { mission_employees } = require("../models/mission_employees.models");
 
 class mission_EmployeesDao {
+    async getAllAssugnedEmployees(req, res, next) {
+        try {
+            const query = `SELECT * FROM mission_employees WHERE active = 'Y' ORDER BY employee_id ASC`;
 
+            const employeesData = await mission_employees.sequelize.query(query, {
+                type: mission_employees.sequelize.QueryTypes.SELECT,
+            });
+
+            if (employeesData && employeesData.length > 0) {
+                res.status(200).json({
+                    success: true,
+                    data: employeesData,
+                    message: "Employees retrieved successfully",
+                });
+            } else {
+                res.status(200).json({
+                    success: false,
+                    data: [],
+                    message: "No active employees found",
+                });
+            }
+        } catch (error) {
+            return next(error);
+        }
+    }
 }
 
 module.exports = mission_EmployeesDao;
