80213487b10bedb13594e3865851233fb0e70830
Akram <akrambenaoun1993@gmail.com>
2025-07-17 00:01:04 +0100

    edit getAllActiveMissions() route

--- routes/routes.js
+++ routes/routes.js
@@ -59,7 +59,7 @@ router.get("/projects/getProjectById/:project_id", projects_controller.getProjec
 router.post("/tasks/getAllTasks/:project_id", tasks_controller.addTask);
 
 // MISSIONS ROUTES
-router.get("/missions/getAllMissions/:params?", missions_controller.getAllActiveMissions);
+router.get("/missions/getAllMissions/:params", missions_controller.getAllActiveMissions);
 router.post("/missions/addMission/", missions_controller.addMission);
 router.get("/missions/getMissionById/:mission_id", missions_controller.getMissionById);
 router.put("/missions/editMission/:mission_id", missions_controller.editMission);
