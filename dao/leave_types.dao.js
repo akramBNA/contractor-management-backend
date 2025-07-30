0ba99c17c9f6bc3e3018cdfc48bb68acc8ab4c88
Akram <akrambenaoun1993@gmail.com>
2025-07-30 12:51:12 +0100

    add leaves_types dao

---
+++ dao/leave_types.dao.js
@@ -0,0 +1,32 @@
+const leave_types = require("../models/leave_types.models");
+
+class leave_typesDao {
+  async getAllleave_types(req, res, next) {
+    try {
+      const get_all_leave_types_query = "SELECT * FROM leave_types WHERE active='Y' ORDER BY leave_type_id ASC";
+      const get_all_leave_types_data = await leave_types.sequelize.query(
+        get_all_leave_types_query,
+        {
+          type: leave_types.sequelize.QueryTypes.SELECT,
+        }
+      );
+      if (get_all_leave_types_data) {
+        res.status(200).json({
+          status: true,
+          data: get_all_leave_types_data,
+          message: "Retrieved successfully",
+        });
+      } else {
+        res.json({
+          status: false,
+          data: [],
+          message: "Failed to retrieve data",
+        });
+      }
+    } catch (error) {
+      return next(error);
+    }
+  }
+}
+
+module.exports = leave_typesDao;
