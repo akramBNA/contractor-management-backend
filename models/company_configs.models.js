5f64f740fa1fd3a3b32a9c1ea54d06f5af8fdc9f
Akram <akrambenaoun1993@gmail.com>
2025-08-09 16:28:15 +0100

    create a company configs DB model

---
+++ models/company_configs.models.js
@@ -0,0 +1,36 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { sequelize } = require("../database/database.js");
+
+const company_configs = sequelize.define(
+  "company_configs",
+  {
+    company_config_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    payroll_cycle_start_day: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    leave_accrual_rate: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    active: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "Y",
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = {
+  company_configs,
+  sequelize,
+};
