4ea8b1f4c9d46458da3e52e4dffbbac834caa6b4
Akram <akrambenaoun1993@gmail.com>
2025-08-17 18:04:13 +0100

    create a get company configs controller

--- controllers/company_configs.controllers.js
+++ controllers/company_configs.controllers.js
@@ -3,4 +3,8 @@ const company_configsDao = require("../dao/company_configs.dao");
 const company_configs_instance = new company_configsDao();
 
 module.exports = {
+
+    getCompanyConfigs: function (req, res, next){
+        company_configs_instance.getCompanyConfigs(req, res, next);
+    },
 };
