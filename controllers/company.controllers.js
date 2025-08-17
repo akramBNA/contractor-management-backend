2c8861e2c440ba14d488b8a555a79bd5fc93b183
Akram <akrambenaoun1993@gmail.com>
2025-08-17 18:10:59 +0100

    create a get company informations controller

--- controllers/company.controllers.js
+++ controllers/company.controllers.js
@@ -3,4 +3,7 @@ const companyDao = require("../dao/company.dao.js");
 const company_instance = new companyDao();
 
 module.exports = {
+    getCompanyInformations: function (req, res, next) {
+        company_instance.getCompanyInformations(req, res, next);
+    },
 };
