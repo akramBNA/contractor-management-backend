d8a854e08445a0972d81ac5bb282158bf6f10a6f
Akram <akrambenaoun1993@gmail.com>
2025-08-29 19:35:10 +0100

    server error fix

--- database/database_supabase_2.js
+++ database/database_supabase_2.js
@@ -1,10 +1,9 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  dialect: "postgres",
   dialectOptions: {
-    ssl: { require: true },
-     rejectUnauthorized: false,
+    ssl: { require: true, rejectUnauthorized: false },
   },
   logging: false,
 });
