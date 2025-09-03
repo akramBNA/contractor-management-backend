01dd4e3d109690281185239ec9ef7585a231b720
Akram <akrambenaoun1993@gmail.com>
2025-09-03 16:48:25 +0100

    add a reset leaves cron job

--- Cron Jobs/leave_crons.js
+++ Cron Jobs/leave_crons.js
@@ -5,7 +5,19 @@ cron.schedule("0 0 1 * *", async () => {
   try {
     console.log("-- Cron job started: Adding leave credits...");
     employeesControllers.addLeaveCreditEveryMonth();
+    console.log("-- Cron job finished successfully!");
   } catch (error) {
     console.log("-- failed to ran this job!");
   }
 });
+
+cron.schedule("0 0 1 1 *", async () => {
+  try {
+    console.log("-- Cron job started: Resetting leave credits...");
+    await employeesControllers.resetLeaveCreditEveryYear();
+    console.log("-- Cron job finished successfully!");
+  } catch (error) {
+    console.error("-- Failed to run this job!", error);
+  }
+});
+
