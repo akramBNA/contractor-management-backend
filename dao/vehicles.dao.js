062da07e574b81e906ad93e7427c276ae23e82fb
Akram <akrambenaoun1993@gmail.com>
2025-09-09 17:49:09 +0100

    no vehicles found confition

--- dao/vehicles.dao.js
+++ dao/vehicles.dao.js
@@ -26,7 +26,11 @@ class vehiclesDao {
           type: vehicles.sequelize.QueryTypes.SELECT,
         }
       );
-      if (!get_all_vehicles_count_data.length) {
+      console.log("-------------------------- get_all_vehicles_count_data", get_all_vehicles_count_data[0].total);
+      console.log("!get_all_vehicles_count_data[0].total", !get_all_vehicles_count_data[0].total);
+      
+      
+      if (get_all_vehicles_count_data[0].total) {
         return res.json({
           success: true,
           data: [],
