bdda1b1f4f454e32861f24ef3c2146e175eb1c80
Akram <akrambenaoun1993@gmail.com>
2025-09-12 20:10:27 +0100

    create an addVehicle controllers

--- controllers/vehicles.controllers.js
+++ controllers/vehicles.controllers.js
@@ -1,8 +1,11 @@
-const  vehiclesDao = require("../dao/vehicles.dao.js");
+const vehiclesDao = require("../dao/vehicles.dao.js");
 const vehicle_instance = new vehiclesDao();
 
 module.exports = {
   getAllVehicles: function (req, res, next) {
     vehicle_instance.getAllVehicles(req, res, next);
-    },
+  },
+  addVehicle: function (req, res, next) {
+    vehicle_instance.addVehicle(req, res, next);
+  },
 };
