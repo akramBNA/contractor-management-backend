3c6f3e28dace528eb4e1bf3a7622911ace08dd04
Akram <akrambenaoun1993@gmail.com>
2025-10-14 21:28:37 +0100

    create an attendance dao file

---
+++ dao/attendances.dao.js
@@ -0,0 +1,8 @@
+const { ca, da } = require("date-fns/locale");
+const { attendances } = require("../models/attendances.models");
+
+class attendancesDao {
+    //
+}
+
+module.exports = attendancesDao;
