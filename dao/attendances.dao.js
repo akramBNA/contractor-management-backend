08a15dbb81bb09de0a3cbb9476bbceeef961da1a
Akram <akrambenaoun1993@gmail.com>
2025-11-01 20:16:23 +0100

    create addCompanyInformations() dao func

--- dao/attendances.dao.js
+++ dao/attendances.dao.js
@@ -2,7 +2,6 @@ const { ca, da } = require("date-fns/locale");
 const { attendances } = require("../models/attendances.models");
 
 class attendancesDao {
-    //
 }
 
 module.exports = attendancesDao;
