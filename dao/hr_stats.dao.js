187a1db3482dd5923f9706df07d10b70f80c2be6
Akram <akrambenaoun1993@gmail.com>
2025-11-25 17:15:53 +0100

    update hr stats func; add a gender count

--- dao/hr_stats.dao.js
+++ dao/hr_stats.dao.js
@@ -1,10 +1,24 @@
-const { employees, sequelize: employeesSequelize } = require("../models/employees.models");
+const {
+  employees,
+  sequelize: employeesSequelize,
+} = require("../models/employees.models");
 
 class hr_statsDao {
-
- async hrStatistics(req, res, next) {
+  async hrStatistics(req, res, next) {
     try {
       const currentMonth = new Date().getMonth() + 1;
+
+      const getGenderDistributionQuery = `SELECT 
+                                              SUM(CASE WHEN employee_gender = 'Male' THEN 1 ELSE 0 END) AS male_count,
+                                              SUM(CASE WHEN employee_gender = 'Female' THEN 1 ELSE 0 END) AS female_count
+                                          FROM employees
+                                          WHERE active = 'Y'`;
+
+      const genderDistributionData = await employees.sequelize.query(getGenderDistributionQuery,
+        {
+          type: employees.sequelize.QueryTypes.SELECT,
+        }
+      );
 
       const get_birthdays_query = `SELECT e.employee_id,
                                         e.employee_name,
@@ -14,31 +28,25 @@ class hr_statsDao {
                                     WHERE EXTRACT(MONTH FROM e.employee_birth_date) = :currentMonth
                                     ORDER BY EXTRACT(DAY FROM e.employee_birth_date) ASC `;
 
-      const birthdaysData = await employees.sequelize.query(get_birthdays_query, {
-        replacements: { currentMonth },
-        type: employees.sequelize.QueryTypes.SELECT,
-      });
-      
-      if (birthdaysData) {
-        res.status(200).json({
+      const birthdaysData = await employees.sequelize.query(get_birthdays_query,
+        {
+          replacements: { currentMonth },
+          type: employees.sequelize.QueryTypes.SELECT,
+        }
+      );
+
+      res.status(200).json({
         success: true,
-        data: birthdaysData,
-        message: birthdaysData.length > 0 
-                ? "Retrieved successfully" 
-                : "No birthdays this month"
-        });
-      } else {
-        res.json({
-          success: false,
-          data: [],
-          message: "Failed to retrieve data",
-        });
-      }
+        data: {
+          birthdaysData: birthdaysData,
+          genderDistributionData: genderDistributionData[0],
+        },
+        message: "HR Stats retrieved successfully",
+      });
     } catch (error) {
       return next(error);
-    }  
- };
-
-};
+    }
+  };
+}
 
 module.exports = hr_statsDao;
