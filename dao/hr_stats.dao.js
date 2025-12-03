1c42228751c29e53ba64b282bfd12fc30682957a
Akram <akrambenaoun1993@gmail.com>
2025-12-03 23:21:37 +0100

    remove unnecessary hr_metrics function

--- dao/hr_stats.dao.js
+++ dao/hr_stats.dao.js
@@ -74,32 +74,6 @@ class hr_statsDao {
     }
   };
 
-  async hrMetrics(req, res, next) {
-    try {
-      const totalEmployeesQuery = `SELECT COUNT(*) AS total_employees
-                                   FROM employees
-                                   WHERE active = 'Y'`;
-
-      const totalEmployeesData = await employees.sequelize.query(totalEmployeesQuery,
-        {
-          type: employees.sequelize.QueryTypes.SELECT,
-        }
-      );
-
-      const totalEmployees = totalEmployeesData[0].total_employees;
-
-      res.status(200).json({
-        success: true,
-        data: {
-          totalEmployees: totalEmployees,
-        },
-        message: "HR Metrics retrieved successfully",
-      });
-
-    } catch (error) {
-      return next(error);
-    }
-  };
 }
 
 module.exports = hr_statsDao;
