b60962adcc6f213bc82b8c6a988f5238797fac22
Akram <akrambenaoun1993@gmail.com>
2026-04-28 19:01:12 +0100

    CREATE an delete company configs controller

--- controllers/company_configs.controllers.js
+++ controllers/company_configs.controllers.js
@@ -3,14 +3,16 @@ const company_configsDao = require("../dao/company_configs.dao");
 const company_configs_instance = new company_configsDao();
 
 module.exports = {
-
-    getCompanyConfigs: function (req, res, next){
-        company_configs_instance.getCompanyConfigs(req, res, next);
-    },
-    createCompanyConfigs: function (req, res, next){
-        company_configs_instance.createCompanyConfigs(req, res, next);
-    },
-    updateCompanyConfigs: function (req, res, next){
-        company_configs_instance.updateCompanyConfigs(req, res, next);
-    },
+  getCompanyConfigs: function (req, res, next) {
+    company_configs_instance.getCompanyConfigs(req, res, next);
+  },
+  createCompanyConfigs: function (req, res, next) {
+    company_configs_instance.createCompanyConfigs(req, res, next);
+  },
+  updateCompanyConfigs: function (req, res, next) {
+    company_configs_instance.updateCompanyConfigs(req, res, next);
+  },
+  deleteCompanyConfigs: function (req, res, next) {
+    company_configs_instance.deleteCompanyConfigs(req, res, next);
+  },
 };
