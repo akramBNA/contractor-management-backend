52bb63091e5c69d9098e85cd8ba828b584daff9e
Akram <akrambenaoun1993@gmail.com>
2026-05-05 23:02:38 +0100

    CREATE getBillingItemsByBillingId() DAO function

--- dao/billing_items.dao.js
+++ dao/billing_items.dao.js
@@ -36,6 +36,38 @@ class billing_itemsDao {
       return next(error);
     }
   }
+
+  async getBillingItemsByBillingId(req, res, next) {
+    try {
+      const billing_id = req.params.billing_id;
+
+      const get_billing_items_by_billing_id_query =
+        "SELECT * FROM billing_items WHERE active='Y' AND billing_id = :billing_id ORDER BY billing_item_id ASC";
+      const get_billing_items_by_billing_id_data =
+        await billing_items.sequelize.query(
+          get_billing_items_by_billing_id_query,
+          {
+            type: billing_items.sequelize.QueryTypes.SELECT,
+            replacements: { billing_id },
+          },
+        );
+      if (get_billing_items_by_billing_id_data) {
+        res.status(200).json({
+          success: true,
+          Data: get_billing_items_by_billing_id_data,
+          message: "Retrieved successfully",
+        });
+      } else {
+        res.json({
+          success: false,
+          Data: [],
+          message: "Failed to retrieve data",
+        });
+      }
+    } catch (error) {
+      return next(error);
+    }
+  }
 }
 
 module.exports = billing_itemsDao;
